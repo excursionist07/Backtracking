c4b34018f380de0fe4893f775a8b897692e1e4e0
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-15 13:52:14 +0530

    Create Generate Parentheses.cpp

---
+++ Generate Parentheses.cpp
@@ -0,0 +1,26 @@
+void printparenthesis(int n,int open,int close,string str,vector<string>&ans)
+    {
+
+     if(close==n)
+     {
+         ans.push_back(str);
+         return;
+     }
+     else
+     {
+      if(open>close)
+        printparenthesis(n,open,close+1,str+')',ans);
+
+      if(open<n)
+        printparenthesis(n,open+1,close,str+'(',ans);
+
+     }
+    }
+    vector<string> generateParenthesis(int n)
+    {
+     vector<string>ans;
+      if(n==0)return ans;
+      printparenthesis(n,0,0,"",ans);
+      return ans;
+    }
+};

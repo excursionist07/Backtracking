198ee2cba6b54f9d8b570013c74f880c05998e26
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-15 13:49:48 +0530

    Create Combination Sum.cpp

---
+++ Combination Sum.cpp
@@ -0,0 +1,23 @@
+vector<vector<int>>ans;
+    void combination(vector<int>& candidates,int target,int sum,int index,vector<int>&res)
+    {
+     if(sum==target)
+     {
+      ans.push_back(res);
+      return;
+     }
+     if(candidates.size()==index || candidates[index]+sum>target)
+         return;
+     res.push_back(candidates[index]);
+     combination(candidates,target,sum+candidates[index],index,res);
+     res.pop_back();
+     combination(candidates,target,sum,index+1,res);
+    }
+    
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
+    {
+     vector<int>res;
+     sort(candidates.begin(),candidates.end());
+     combination(candidates,target,0,0,res);
+     return ans;
+    }

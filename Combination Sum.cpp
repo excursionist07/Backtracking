1f245a0a9058fdd6eba80414e2e061d4adc45c86
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-25 16:30:14 +0530

    Update Combination Sum.cpp

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -29,6 +29,49 @@ public:
 
 // 40. Combination Sum II
 
+// 1st method
+
+class Solution {
+public:
+    set<vector<int>>ss;
+    void doit(vector<vector<int>>&ans,vector<int>&res,vector<int>&candidates,int tar,int idx)
+    {
+     if(tar<0)
+         return;
+     if(tar==0)
+     {
+      //ans.push_back(res);
+      ss.insert(res);
+      return;
+     }
+     for(int i=idx;i<candidates.size();i++)
+     {
+      //if(i>idx && candidates[i]==candidates[i-1])
+         // continue;
+      res.push_back(candidates[i]);
+      doit(ans,res,candidates,tar-candidates[i],i+1);
+      res.pop_back();
+     }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+     ss.clear();
+     int n=candidates.size();
+     vector<vector<int>>ans;
+     vector<int>res;
+     if(n==0)
+         return ans;
+     sort(candidates.begin(),candidates.end());
+     doit(ans,res,candidates,target,0);
+     for(auto xx:ss)
+         ans.push_back(xx);
+     return ans;
+    }
+};
+
+// 2nd method
+
 class Solution {
 public:
     void doit(vector<vector<int>>&ans,vector<int>&res,vector<int>&candidates,int tar,int idx)
@@ -42,7 +85,7 @@ public:
      }
      for(int i=idx;i<candidates.size();i++)
      {
-      if(i>idx && candidates[i]==candidates[i-1])
+      if(i>idx && candidates[i]==candidates[i-1])// for not storing duplicate answer
           continue;
       res.push_back(candidates[i]);
       doit(ans,res,candidates,tar-candidates[i],i+1);
@@ -62,3 +105,5 @@ public:
      return ans;
     }
 };
+
+

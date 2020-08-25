934382ca164e71737859b2a867a2f20c06bb56d1
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-08-25 15:16:40 +0530

    Update Combination Sum.cpp

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -26,3 +26,39 @@ public:
      return ans;
     }
 };
+
+// 40. Combination Sum II
+
+class Solution {
+public:
+    void doit(vector<vector<int>>&ans,vector<int>&res,vector<int>&candidates,int tar,int idx)
+    {
+     if(tar<0)
+         return;
+     if(tar==0)
+     {
+      ans.push_back(res);
+      return;
+     }
+     for(int i=idx;i<candidates.size();i++)
+     {
+      if(i>idx && candidates[i]==candidates[i-1])
+          continue;
+      res.push_back(candidates[i]);
+      doit(ans,res,candidates,tar-candidates[i],i+1);
+      res.pop_back();
+     }
+    }
+    
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) 
+    {
+     int n=candidates.size();
+     vector<vector<int>>ans;
+     vector<int>res;
+     if(n==0)
+         return ans;
+     sort(candidates.begin(),candidates.end());
+     doit(ans,res,candidates,target,0);
+     return ans;
+    }
+};

7a0726c993555fb59d31f603575b9b7666337769
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-09-15 14:10:38 +0530

    Update Combination Sum.cpp

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -132,4 +132,31 @@ public:
     }
 };
 
+// 377. Combination Sum IV
+
+class Solution {
+public:
+    int combinationSum4(vector<int>& nums, int target) 
+    {
+     int n=nums.size();
+     if(target==0)
+         return 1;
+     if(n==0)
+         return 0;
+     unsigned int dp[target+1];
+     for(int i=0;i<=target;i++)
+         dp[i]=0;
+     dp[0]=1;
+     for(int i=1;i<=target;i++)
+     {
+      for(int j=0;j<n;j++)
+      {
+       if(i-nums[j]>=0)
+           dp[i]+=dp[i-nums[j]];
+      }
+     }
+     return dp[target];
+    }
+};
+
 

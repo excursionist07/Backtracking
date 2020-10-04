28facda90c4e40f1373b539507de83c8944a1a6a
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2020-10-05 01:56:31 +0530

    Update Gray Code.cpp

--- Gray Code.cpp
+++ Gray Code.cpp
@@ -32,3 +32,16 @@ public:
      
     }
 };
+
+/*
+1) Let the list of (n-1)-bit Gray codes be L1. Create another list L2 which is reverse of L1. 
+2) Modify the list L1 by prefixing a ‘0’ in all codes of L1. 
+3) Modify the list L2 by prefixing a ‘1’ in all codes of L2. 
+4) Concatenate L1 and L2. The concatenated list is required list of n-bit Gray codes.
+For example, following are steps for generating the 3-bit Gray code list from the list of 2-bit Gray code list. 
+L1 = {00, 01, 11, 10} (List of 2-bit Gray Codes) 
+L2 = {10, 11, 01, 00} (Reverse of L1) 
+Prefix all entries of L1 with ‘0’, L1 becomes {000, 001, 011, 010} 
+Prefix all entries of L2 with ‘1’, L2 becomes {110, 111, 101, 100} 
+Concatenate L1 and L2, we get {000, 001, 011, 010, 110, 111, 101, 100}
+*/

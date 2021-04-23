7417db70d9036ffec6e9cd527689bfc70509e0e0
Shubham Kumar <49623544+excursionist07@users.noreply.github.com>
2021-04-23 21:08:03 +0530

    Update 17. Letter Combinations of a Phone Number.cpp

--- 17. Letter Combinations of a Phone Number.cpp
+++ 17. Letter Combinations of a Phone Number.cpp
@@ -36,4 +36,51 @@ public:
     }
 };
 
+// 2nd method --> iterative solution
+
+class Solution {
+public:
+    void doit(string digits,vector<string>dict,vector<string>&vv)
+    {
+     queue<string>q;
+     q.push("");
+     while(!q.empty())
+     {
+      string temp=q.front();
+      q.pop();
+      if(temp.length()==digits.length())
+          vv.push_back(temp); // when temp has characters from all digits;
+      else
+      {
+        for(auto xx:dict[digits[temp.length()]-'0'])
+            q.push(temp+xx);
+      }
+     }
+    }
+    
+    vector<string> letterCombinations(string digits)
+    {
+     int n=digits.length();
+     if(n==0)
+         return {};
+     vector<string>vv;
+     vector<string>dict;
+     dict.push_back("");
+     dict.push_back("");
+     dict.push_back("abc");
+     dict.push_back("def");
+     dict.push_back("ghi");
+     dict.push_back("jkl");
+     dict.push_back("mno");
+     dict.push_back("pqrs");
+     dict.push_back("tuv");
+     dict.push_back("wxyz");
+     doit(digits,dict,vv);
+     return vv;
+     
+     
+    }
+};
+
 // Time-O(3^N*4^M) where N is the number of digits in the input that maps to 3 letters (e.g. 2, 3, 4, 5, 6, 8) and M is the number of digits in the input that maps to 4 letters (e.g. 7, 9)
+
